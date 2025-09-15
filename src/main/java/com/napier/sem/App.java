e2015aaf8772b794e11156164bf1e4e7bc6bc316
Lin-Myat-Thu <40794502@live.napier.ac.uk>
2025-09-15 15:40:01 +0630

    lab_02 testing

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -4,5 +4,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Changes username");
+        System.out.println("Lab_02 testing");
     }
 }
