e8968f57c1a13f4dfec2b448c27dd7d1a05e5946
Lin-Myat-Thu <40794502@live.napier.ac.uk>
2025-09-23 00:23:02 +0630

    Solving Issue6

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -5,9 +5,17 @@ import java.sql.*;
 public class App {
     public static void main(String[] args) {
         // Create new Application
-        App app = new App();
-        app.connect();
-        app.disconnect();
+        App a = new App();
+
+        // Connect to database
+        a.connect();
+        // Get Employee
+        Employee emp = a.getEmployee(255530);
+        // Display results
+        a.displayEmployee(emp);
+
+        // Disconnect from database
+        a.disconnect();
     }
     /**
      * Connection to MySQL database.
@@ -56,6 +64,65 @@ public class App {
             catch (SQLException e){
                 System.out.println("Error closing connection to database");
             }
+        }
+    }
+
+    public Employee getEmployee(int ID){
+        try{
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String query =
+                    "SELECT e.emp_no, e.first_name, e.last_name, " +
+                            "t.title, " +
+                            "s.salary, " +
+                            "d.dept_name, " +
+                            "CONCAT(mgr.first_name, ' ', mgr.last_name) AS manager " +
+                            "FROM employees e " +
+                            "JOIN titles t ON e.emp_no = t.emp_no AND t.to_date = '9999-01-01' " +
+                            "JOIN salaries s ON e.emp_no = s.emp_no AND s.to_date = '9999-01-01' " +
+                            "JOIN dept_emp de ON e.emp_no = de.emp_no AND de.to_date = '9999-01-01' " +
+                            "JOIN departments d ON de.dept_no = d.dept_no " +
+                            "JOIN dept_manager dm ON de.dept_no = dm.dept_no AND dm.to_date = '9999-01-01' " +
+                            "JOIN employees mgr ON dm.emp_no = mgr.emp_no " +
+                            "WHERE e.emp_no = " + ID;
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(query);
+            // Return new employee if valid.
+            // Check one is returned
+            if(rset.next()){
+                Employee emp = new Employee();
+                emp.emp_no = rset.getInt("emp_no");
+                emp.first_name = rset.getString("first_name");
+                emp.last_name = rset.getString("last_name");
+                emp.title = rset.getString("title");
+                emp.salary = rset.getInt("salary");
+                emp.dept_name = rset.getString("dept_name");
+                emp.manager = rset.getString("manager");
+                return emp;
+            }
+            else {
+                return null;
+            }
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get employee details");
+            return null;
+        }
+    }
+    public void displayEmployee(Employee emp)
+    {
+        if (emp != null)
+        {
+            System.out.println(
+                    emp.emp_no + " "
+                            + emp.first_name + " "
+                            + emp.last_name + "\n"
+                            + emp.title + "\n"
+                            + "Salary:" + emp.salary + "\n"
+                            + emp.dept_name + "\n"
+                            + "Manager: " + emp.manager + "\n");
         }
     }
 
